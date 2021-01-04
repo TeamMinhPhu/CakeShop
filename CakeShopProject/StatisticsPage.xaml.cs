c9973b21adfe131c1c338d82be3e9f6b2978e207
Phu Do <dophu1402@gmail.com>
2021-01-04 21:18:19 +0700

    ..

--- CakeShopProject/StatisticsPage.xaml.cs
+++ CakeShopProject/StatisticsPage.xaml.cs
@@ -53,9 +53,13 @@ namespace CakeShopProject
 		private void Page_Loaded(object sender, RoutedEventArgs e)
 		{
 			YearComboBox();
+			cakeTypeChart();
 			this.DataContext = this;
 		}
 
+		/// <summary>
+		/// combobox for selecting year
+		/// </summary>
 		private void YearComboBox()
 		{
 			int year = DateTime.Now.Year;
@@ -72,14 +76,17 @@ namespace CakeShopProject
 			YearForMonthlyChart.SelectedIndex = 0;
 		}
 
+		/// <summary>
+		/// Bar chart
+		/// </summary>
 		private void SetupMonthlyChart()
 		{
 			List<long> Revenue = new List<long>();
 			int selectedYear = _current_year - YearForMonthlyChart.SelectedIndex;
 			var bills = db.BILLs.Where(c => c.COMPLETED_DATE.Value.Year == selectedYear && c.STATUS == 2).Select(c => new{c.BILL_ID, c.COMPLETED_DATE } );
-			if (bills.Count() == 0)
+			if (bills.Count() == 0 || bills == null)
 			{
-				MonthlyChart.Clear();
+				MonthlyChart.Clear(); //erase old data
 			}
 			else
 			{
@@ -101,8 +108,9 @@ namespace CakeShopProject
 							totalPrice += (long)cake.PRICE * (long)cake.QUANTITY;
 						}
 						catch { /*do nothing*/ }
-						Revenue.Add(totalPrice / 1000);
+
 					}
+					Revenue.Add(totalPrice / 1000);
 
 				}
 
@@ -111,7 +119,7 @@ namespace CakeShopProject
 				{
 					Values = new ChartValues<long>(Revenue),
 				};
-				MonthlyChart.Clear();
+				MonthlyChart.Clear(); //erase old data
 				MonthlyChart.Add(newBar);
 			}
 		}
@@ -119,8 +127,33 @@ namespace CakeShopProject
 		private void YearForMonthlyChart_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			_selected_year = _current_year - YearForMonthlyChart.SelectedIndex;
-		
 			SetupMonthlyChart();
+		}
+
+		private void cakeTypeChart()
+		{
+			var types = db.TYPEs.ToList();
+			var bills = db.BILLs.Where(c => c.STATUS == 2).Select(c => c.BILL_ID).ToList();
+			var billDetails = db.BILLDETAILs.Where(c => bills.Contains(c.BILL_ID)).ToList();
+
+			TypeChart.Clear(); //erase old data
+			foreach (var type in types)
+			{
+				long total = 0;
+				var cakebills = billDetails.Where(c => c.CAKE.TYPE_ID == type.TYPE_ID);
+				foreach (var cakebill in cakebills)
+				{
+					total += (long)cakebill.PRICE * (int)cakebill.QUANTITY;
+				}
+
+				var newPie = new PieSeries()
+				{
+					Title = type.TYPE_NAME,
+					Values = new ChartValues<long> { total }
+				};
+
+				TypeChart.Add(newPie);
+			}
 		}
 	}
 }
